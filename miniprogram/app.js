d5f0b1a4e169c251d20ce23c4c3c7273e40e445e
Li-YuXinxin <lyx040323lyx@gmail.com>
2026-09-08 11:17:30 +0800

    feat: 完成首页功能卡片开发，支持点击跳转至虫害识别、模型训练、首页、知识库管理和智能问答五个tabBar页面

---
+++ miniprogram/app.js
@@ -0,0 +1,6 @@
+// app.js
+App({
+  globalData: {
+    apiBase: 'http://localhost:8000'
+  }
+})
---
+++ miniprogram/pages/home/home.js
@@ -0,0 +1,40 @@
+// home/home.js
+
+// 获取 app 实例
+const app = getApp()
+
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+  data: {
+    features: [
+      { title: '模型训练', url: '/pages/train/train' },
+      { title: '稠密识别', url: '/pages/classify/classify' },
+      { title: '知识库管理', url: '/pages/knowledge/knowledge' },
+      { title: '智能问答', url: '/pages/chat/chat' }
+    ]
+  },
+
+  navigateTo(e) {
+    // 获取点击的 url
+    const url = e.currentTarget.dataset.url
+    if(!url)  return
+
+    const tarBarPages = [
+      "pages/classify/classify",
+      "pages/knowledge/knowledge",
+      "pages/train/train",
+      "pages/chat/chat"
+    ]
+
+    if (tarBarPages.includes(url)) {
+        // 如果点击的是页面
+        wx.switchTab({ url })
+    } else {
+        // 点击导航栏
+        wx.navigateTo({ url })
+    }
+  }
+})
